e3038e434e31e1df84af0a93f0598ebf19abfb02
Mingjie Jiang <jiang@mingjie.info>
2018-03-10 23:16:45 -0500

    Create template

---
+++ assets/js/site.js
@@ -0,0 +1,62 @@
+// Site Methods Declaration
+// Written by Mingjie Jiang (@mj66) - MIT License
+
+Handlebars.registerHelper('attr', function (name, data) {
+    if (typeof target === 'undefined') target = "";
+
+    var result = ' ' + name + '="' + data + '" ';
+
+    return new Handlebars.SafeString(result);
+});
+
+Handlebars.registerHelper("nextMeeting", function () {
+    if (config.meeting.overrideTime == "") {
+        return getNextDayOfWeek(new Date(), config.meeting.dow);
+    } else {
+        return config.meeting.overrideTime;
+    }
+});
+
+Handlebars.registerHelper("getMaterials", function () {
+    if (config.meeting.material.text == "") {
+        return "Download Material"
+    } else {
+        return config.meeting.material.text;
+    }
+});
+
+Handlebars.registerHelper('if', function (conditional, options) {
+    if (conditional) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+Handlebars.registerHelper('each', function (context, options) {
+    var ret = "";
+
+    for (var i = 0, j = context.length; i < j; i++) {
+        ret = ret + options.fn(context[i]);
+    }
+
+    return ret;
+});
+
+var main = document.getElementById("site-main").innerHTML;
+var meta = document.getElementById("site-meta").innerHTML;
+var loadMain = Handlebars.compile(main);
+var loadMeta = Handlebars.compile(meta);
+
+var siteHtml = loadMain(config);
+var metaHtml = loadMeta(config);
+
+document.getElementById("site").innerHTML = siteHtml;
+document.getElementsByTagName('head')[0].innerHTML += metaHtml;
+
+function getNextDayOfWeek(date, dayOfWeek) {
+    // Code to check that date and dayOfWeek are valid left as an exercise ;)
+    var resultDate = new Date(date.getTime());
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+    return resultDate.toDateString();
+}
---
+++ club.config.js
@@ -0,0 +1,79 @@
+// Your website's configurations and customizations 
+// Do not edit any other files in this repository other than
+// this file if you don't understand what you are doing
+
+// Watch out for the quotation marks when modifying - do not
+// delete them! You'll break the entire file structure.
+
+// Written by Mingjie Jiang (@mj66) - MIT License
+
+var config = {
+    meta: {
+        club: "Wootton Hack Club", //Name of your club
+        domain: "hack", //Your club domain, for example, if "site.wootton.club", put "site"
+        shortDesc: "The best club on the planet", //Brief description about your club. BRIEF!
+
+        // Your club description. If you need want to break lines, append a forward slash
+        // right where the line breaks
+        desc: "Wootton Hack Club is a new club starting in 2017 at Wootton High School. \
+                Members will explore topics in different computer science fields such as \
+                game development, algorithms, open source software or other smaller code \
+                snippets. They will learn and communicate in order to freely create their \
+                own projects, and conduct researches related to the computer engineering \
+                field.",
+        
+        hasLogo: false, //Show your club logo above the "about" section
+        logoUrl: "" //A direct link to your logo. Try uploading your image to imgur.com
+    },
+
+    meeting: {
+        dow: 3, //Date of week [1,7] - Monday=1, Friday=5
+        overrideTime: "", //Overridden meeting time
+        
+        room: "Room 262", //Your meeting location
+
+        showRemind: true, //Show a link to allow people sign up for Remind.com
+        remindLink: "https://www.remind.com/join/whs-hack", //Your Remind.com link
+        
+        topic:"Work on workshops and learn some more Python", //Topic of your meeting
+        hasMaterial: false, //Set to false to hide download material button
+        material: {
+            link: "", //Link to meeting materials, use Google Drive to host the file
+            text: "" //Customized text to meeting materials
+        }
+    },
+
+    announcement: [
+        //Copy and paste this block from { to }, to create a new announcement
+        //Don't forget about the comma!
+        //Contents are HTML enabled.
+        {
+            content: "Please bring your laptops and install Python 3.x for the next meeting!",
+            date: "3/10"
+        },
+    ],
+
+    //Display an image banner on top of the page
+    banner: {
+        show: true, //Set to false to hide banner
+
+        //A direct link to your banner image. 
+        //Try uploading your image to imgur.com and get the link.
+        imageUrl: "https://mingjie.info/wp-content/uploads/2018/02/upload-1516256672-1024x616.png"
+    },
+
+    customize: {
+        siteColor: "#e42d42;", //Site primary color in hex
+
+        //Navbar customization
+        navbar: {
+            colored: true, //Color navbar with primary color
+            textWhite: true //Make navbar title white
+        },
+    },
+
+    master: { //Master Data: Do not change
+        version: "0.1.0",
+        link: "https://get.wootton.club"
+    }
+}
